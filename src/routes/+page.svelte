7031dd39fc1d77025ffdef6aa003619df1ee8b4f
Snowflake6413 <verycoolandsmart@protonmail.com>
2025-12-27 13:12:53 -0500

    add: some buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,14 @@
         gap: 2em;
         justify-content: center;
     }
+    :global(body){
+        background-color: #7BC4C4;
+    }
+    .buttons {
+        display: flex;
+        justify-content: center;
+        gap: 1em;
+    }
 </style>
 
 
@@ -25,4 +33,18 @@
 <p>my name is {name}! i am a 15 year old male :3 </p>
 <p>im a proud hackclubber!</p>
 <p>i do alot of stuff!</p>
+</div>
+
+<div class=buttons>
+<img src="https://88x31.nl/gifs/beecage.gif" alt="buzz buzz" height="31" width="88">
+<img src="https://88x31.nl/gifs/blends.gif" alt="erm.." height="31" width="88">
+<a href="https://bitwarden.com">
+<img src="https://88x31.nl/gifs/bitwarden.gif" alt="bitwarden is cool" height="31" width="88">
+</a>
+<a href="http://youtu.be/xvFZjo5PgG0">
+<img src="https://88x31.nl/gifs/brothings.gif" alt="Wtf is that yaoi" height="31" width="88">
+</a>
+<a href="https://hackclub.com">
+<img src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/d07b324a45721813_button__2_.gif" alt="HACK CLUB!!" height="31" width="88">
+</a>
 </div>
