ccec76c88990e7d78d38bb081da08f85941f2ed8
Snowflake6413 <verycoolandsmart@protonmail.com>
2026-03-19 18:14:49 -0400

    goo goo gaga

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-cloudflare for deployment on Cloudflare Pages
+		// adapter-vercel for deployment on Vercel
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs22.x'
+		}),
 		// Required for PostHog session replay to work correctly with SSR
 		paths: {
 			relative: false
